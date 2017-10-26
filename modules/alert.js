74dac059dccf524982d9228e3f6495ba15c7faee
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2017-10-26 01:56:39 +0100

    tabs

--- modules/alert.js
+++ modules/alert.js
@@ -3,37 +3,37 @@
 const electron = require("electron");
 
 function object_to_string(o) {
-    let msg = JSON.stringify(o);
-    return msg;
+	let msg = JSON.stringify(o);
+	return msg;
 }
 
 function alert_main(msg) {
-    electron.dialog.showMessageBox({
-        message: msg.toString(),
-        title: "Alert",
-        buttons: ["OK"]
-    }, () => {});               // Providing a callback makes the window not block the process
+	electron.dialog.showMessageBox({
+		message: msg.toString(),
+		title: "Alert",
+		buttons: ["OK"]
+	}, () => {});			// Providing a callback makes the window not block the process
 }
 
 function alert_renderer(msg) {
-    electron.remote.dialog.showMessageBox({
-        message: msg.toString(),
-        title: "Alert",
-        buttons: ["OK"]
-    }, () => {});
+	electron.remote.dialog.showMessageBox({
+		message: msg.toString(),
+		title: "Alert",
+		buttons: ["OK"]
+	}, () => {});
 }
 
 module.exports = (msg) => {
-    if (typeof(msg) === "object") {
-        msg = object_to_string(msg);
-    }
-    if (typeof(msg) === "undefined") {
-        msg = "undefined";
-    }
-    msg = msg.trim()
-    if (process.type === "renderer") {
-        alert_renderer(msg);
-    } else {
-        alert_main(msg);
-    }
+	if (typeof(msg) === "object") {
+		msg = object_to_string(msg);
+	}
+	if (typeof(msg) === "undefined") {
+		msg = "undefined";
+	}
+	msg = msg.trim()
+	if (process.type === "renderer") {
+		alert_renderer(msg);
+	} else {
+		alert_main(msg);
+	}
 }
--- modules/proc.js
+++ modules/proc.js
@@ -3,17 +3,17 @@
 // All this is so simple that one should simply do the required test in the caller instead.
 
 exports.is_main = () => {
-    return process.type === "browser";  // For whatever reason, the "main" process is called "browser".
+	return process.type === "browser";	// For whatever reason, the "main" process is called "browser".
 }
 
 exports.is_renderer = () => {
-    return process.type === "renderer";
+	return process.type === "renderer";
 }
 
 exports.get_type = () => {
-    if (process.type === "browser") {
-        return "main";
-    } else {
-        return process.type;
-    }
+	if (process.type === "browser") {
+		return "main";
+	} else {
+		return process.type;
+	}
 }
